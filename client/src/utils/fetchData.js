432c67324bfdce635672354a07dbf7213dd5928b
Yana Seniuk <senyukyana26@gmail.com>
2025-04-19 04:02:41 +0200

    Change style, structure  due to new theme. Deleted pages

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,5 +1,5 @@
 export async function fetchData(url) {
-  const response = await fetch(url, option);
+  const response = await fetch(url);
   if (!response.ok) throw new Error("Failed to fetch data from the server");
   return response.json();
 }
